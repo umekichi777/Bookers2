84b5ed2e94c69ec9ccbc19e0ffc02aaefabe101f
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-02-20 16:47:10 +0000

    [Finish] Q-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
 
   def show
     @books = Book.all
+    @book = Book.new
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @xbooks = @user.books
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
