d7bbbf31a4a0b7450db7372f12c167bb41bd5f9d
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-02-22 09:04:52 +0000

    [Finish] H-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(resource)
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 
